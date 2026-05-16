14763720b9c4dc9aa814bf1bc6ad122ac405b754
Ohayama-alt <ohayamapw@gmail.com>
2026-05-16 12:30:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,26 +16,6 @@ const scores = {
     pxg: 0
 };
 
-
-
-/* =========================
-   EMBARALHAR BOTÕES
-========================= */
-
-questions.forEach(question => {
-
-    const buttons =
-        Array.from(question.querySelectorAll("button"));
-
-    buttons.sort(() => Math.random() - 0.5);
-
-    buttons.forEach(button => {
-        question.appendChild(button);
-    });
-});
-
-
-
 function answer(team) {
 
     scores[team]++;
@@ -57,8 +37,6 @@ function answer(team) {
         showResult();
     }
 }
-
-
 
 function showResult() {
 
@@ -103,7 +81,7 @@ function showResult() {
                 "BASTARD MÜNCHEN";
 
             image.src =
-                "https://us.oricon-group.com/upimg/detail/1000/1578/img660/6e347dace14915a1c1e6e7a9c1b5a27c.jpg";
+                "https://us.oricon-group.com/upimg/sns/1000/1578/img1200/6b9f62fe119a62b30dfa3cc221f804fd.jpg";
 
             text.innerText =
                 "Devore e destrua a coroa do seu time, monte o SEU império.";
@@ -129,7 +107,7 @@ function showResult() {
                 "FC BARCHA";
 
             image.src =
-                "https://us.oricon-group.com/upimg/sns/1000/1578/img1200/6b9f62fe119a62b30dfa3cc221f804fd.jpg";
+                "https://us.oricon-group.com/upimg/detail/1000/1578/img660/fe5114d8770a3b59b23716f0ba87f6bb.jpg";
 
             text.innerText =
                 "Seja imprevisivel, criativo, demonstre do por que você chegou até aqui, se torne em SEU demônio.";
@@ -142,7 +120,7 @@ function showResult() {
                 "Ubers";
 
             image.src =
-                "https://us.oricon-group.com/upimg/detail/1000/1578/img660/fe5114d8770a3b59b23716f0ba87f6bb.jpg";
+                "https://us.oricon-group.com/upimg/detail/1000/1578/img660/5ffd301242839ecb23912dda906db02e.jpg";
 
             text.innerText =
                 "Seja racional, jogue em time, demonstre suas capacidades em grupo, seja racional.. Mostre o SEU intelecto.";
@@ -155,7 +133,7 @@ function showResult() {
                 "PARIS X GEN";
 
             image.src =
-                "https://us.oricon-group.com/upimg/detail/1000/1578/img660/5ffd301242839ecb23912dda906db02e.jpg";
+                "https://us.oricon-group.com/upimg/detail/1000/1578/img660/6e347dace14915a1c1e6e7a9c1b5a27c.jpg";
 
             text.innerText =
                 "Seja uma estrela, jogue como uma pessoa imprevisivel, demonstre seu brilho individual, seja egoista e demonstre o SEU verdadeiro ego.";
